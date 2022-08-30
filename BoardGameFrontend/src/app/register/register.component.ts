4f2f0228e8e2774e55eb15e92d0d10243d29a1a1
Mateusz Janczak <julsonem@gmail.com>
2022-08-30 14:02:34 +0200

    register form

--- BoardGameFrontend/src/app/register/register.component.ts
+++ BoardGameFrontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  public registerForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required])
+    });
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.registerForm.controls[controlName].hasError(errorName);
   }
 
 }
