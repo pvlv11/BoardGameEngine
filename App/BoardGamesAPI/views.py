707cbf4855d9d988302e18c898ffee33ae5a75eb
Mrsenior12 <61933232+Mrsenior12@users.noreply.github.com>
2022-08-30 21:39:29 +0200

    woringURL

---
+++ App/BoardGamesAPI/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+import psycopg2 as postgre
+
+from os import environ
+
+# Create your views here.
+
+# wyswietl wszytskie gry
+def getAllGames(request):
+    # stri="hello world. You are at the polls index"+str(request)
+   # return JsonResponse({"R-Va":"Slawek"})
+
+    
+    postgre_connection = postgre.connect(database=environ.get('POSTGRES_NAME'), 
+                                        user=environ.get('POSTGRES_USER'),
+                                        password=environ.get('POSTGRES_PASSWORD'), 
+                                        host=environ.get('POSTGRES_HOST'), port='5432')
+
+    postgr_cur = postgre_connection.cursor()
+    postgr_cur.execute('select * from t_game where id < 50')
+    jsone = {}
+    j = 0
+    for i in postgr_cur.fetchall():
+        print(i)
+        jsone[j] = i
+        j += 1
+    # jsone={"wszystkie":"gry"}
+    return JsonResponse(jsone)  # HttpResponse(stri)
+    
+    
