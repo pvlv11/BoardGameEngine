8ebe1d61d3c7971e0a9536456c8a0494d043a950
Mrsenior12 <raymanPL5@gmail.com>
2022-11-19 20:49:25 +0100

    new dataset + major changes in population stcript and models

--- App/BoardGamesAPI/urls.py
+++ App/BoardGamesAPI/urls.py
@@ -7,7 +7,10 @@ from . import views
 urlpatterns=[
     path('games/getAllGames',views.getAllGames,name='games/getAllGames'),
     path('games/top_10_games',views.top_10_games,name='games/top_10_games'),
-    path('games/games_review',views.games_review,name='games/games_review'),
+    
+    path('games/get_games_review',views.get_games_review,name='games/get_games_review'),
+    path('games/add_del_review',views.add_del_review,name='games/add_del_review'),
+
     path('games/populate',views.populateDataBase,name='games/populateDataBase'),
     path('games/search_by_string',views.search_by_string,name='games/search_by_string'),
     path('user/register_user',views.register_user2,name='user/register_user'),
@@ -16,4 +19,3 @@ urlpatterns=[
     path('user/check_user_status',views.check_user_status,name='user/check_user_status')
     
     ]
-    
